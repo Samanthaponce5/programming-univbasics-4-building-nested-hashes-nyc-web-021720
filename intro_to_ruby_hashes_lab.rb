32ec5507d5f183aef81c8ddaa01f46c5a97202b9
Samantha Ponce <samanthaponce5@gmail.com>
2020-01-20 02:21:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,14 @@ def monopoly_with_third_tier
   	  key2: "",
   	  key3: "",
   	  key4: ""
-      	}, names:""
+      	}, 
+      	names:{
+      	  key5:"",
+      	  key6:"",
+      	  key7:"",
+      	  key8:""
+      	  
+      	}
       	
   	  }
   	}
